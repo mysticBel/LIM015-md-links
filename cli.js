8facb78a938f1a47eb8d88f8c94a2c87dd19e086
mysticBel <dnxbel@gmail.com>
2021-10-15 01:44:34 -0500

    changing some responses in cli.js, makes more sense now n_n !

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
+#!/usr/bin/env node
 const { mdLinks } = require('./mdlinks.js');
-const { totalLinks, uniqueLinks, brokenLinks, helpMsg, helloMsgValidate,helloMsgStats, helloMsgValidateAndStats,footerMsg, msgErrorRoute, msgNotLinks } = require('./cli-stats.js');
+const { totalLinks, uniqueLinks, brokenLinks, helpMsg, helloMsgValidate,helloMsgStats, helloMsgValidateAndStats,footerMsg, msgErrorRoute, msgNotLinks ,commandInvalidMsg } = require('./cli-stats.js');
 const chalk = require('chalk');
 
 
@@ -13,10 +14,11 @@ const help = option.includes('--help'&& '--h' );
 
 if(option.length === 1) {
     mdLinks(userPath, { validate:false } )
-    .then(res => console.log(res))
+    .then(res =>  {console.log(res) })
     .catch((rej) => {
         if(rej === 'This path does not exist') {
-            console.log(chalk.magentaBright.bold(msgErrorRoute));
+            console.log(chalk.green(msgNotLinks));
+            console.log(chalk.magentaBright.bold(msgErrorRoute));    
             console.log(chalk.cyanBright(helpMsg))
         } else {
             console.log(chalk.green(msgNotLinks));
@@ -27,34 +29,36 @@ if(option.length === 1) {
     if(validate && stats) {
         mdLinks(userPath, {validate:true})
         .then(res => {
-            console.table(chalk.green(helloMsgValidateAndStats))
+            console.table(chalk.blueBright(helloMsgValidateAndStats))
             console.table(chalk.cyanBright(totalLinks(res)))
             console.table(chalk.yellowBright(uniqueLinks(res)))
             console.table(chalk.red(brokenLinks(res)))
-            console.table(chalk.green(footerMsg))})
-        .catch(()=>console.log(chalk.greenBright(msgErrorRoute)))
+            console.table(chalk.blueBright(footerMsg))})
+        .catch(()=>console.log(chalk.magentaBright.bold(msgErrorRoute)))
     }else if (validate) {
         mdLinks(userPath, {validate:true})
         .then(res => {
             console.table(chalk.cyanBright(helloMsgValidate))
             console.log(res)
-            console.table(chalk.cyanBright(footerMsg))})
-        .catch(()=>console.log(chalk.greenBright(msgErrorRoute)))
+            console.table(chalk.cyanBright(footerMsg))})    
+        .catch(()=>console.log(chalk.magentaBright.bold(msgErrorRoute)))
+      
     }else if(stats){
         mdLinks(userPath, {validate:true})
         .then(res => {
-            console.table(chalk.blue(helloMsgStats))
+            console.table(chalk.green(helloMsgStats))
             console.table(chalk.cyanBright(totalLinks(res)))
             console.table(chalk.yellowBright(uniqueLinks(res)))
-            console.table(chalk.blue(footerMsg))})
-        .catch(()=>console.log(chalk.greenBright(msgErrorRoute)))
+            console.table(chalk.green(footerMsg))})
+        .catch(()=>console.log(chalk.magentaBright.bold(msgErrorRoute)))
     }else if(help){
         mdLinks(userPath, { validate:true })
         .then( console.log(chalk.cyanBright(helpMsg)))
-        .catch(()=>console.log(chalk.greenBright(msgErrorRoute)))
+        .catch(()=>console.log(chalk.magentaBright.bold(msgErrorRoute)))
     }else{
        mdLinks(userPath, { validate:true })
-        .then( console.log(chalk.cyanBright(helpMsg)))
-        .catch(()=>console.log(chalk.greenBright(msgErrorRoute)))
+        .then( console.log(chalk.red(commandInvalidMsg)),
+               console.log(chalk.cyanBright(helpMsg)) )
+        .catch(()=>console.log(chalk.magentaBright.bold(msgErrorRoute)))
     }
 }
